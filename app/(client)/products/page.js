d5f631f7a9d2606a6dc55b0f33fd7b549c739d97
kimkim168 <KimKim168>
2024-12-10 23:40:14 +0700

    Up-MyBreadCrumb

--- app/(client)/products/page.js
+++ app/(client)/products/page.js
@@ -1,9 +1,12 @@
+import MyBreadCrumb from "@/components/my-bread-crumb";
 import React from "react";
 
 export default function MyProduct() {
   return (
     <>
-      <p>Hi</p>
+      <section className="max-w-screen-2xl mb-10 mx-auto px-2 xl:px-20 mt-5 ">
+        <MyBreadCrumb />
+      </section>
     </>
   );
 }

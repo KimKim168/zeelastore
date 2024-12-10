76a6e265609a0f17c73e2c25c196352adf466c52
kimkim168 <KimKim168>
2024-12-10 23:51:26 +0700

    up-MyGallery2

--- components/MyGallery.js
+++ components/MyGallery.js
@@ -1,5 +1,4 @@
 "use client";
-
 import { useEffect, useState } from "react";
 import GLightbox from "glightbox";
 import "glightbox/dist/css/glightbox.min.css";
@@ -64,23 +63,21 @@ const MyGallery = ({
         <Carousel>
           <CarouselContent>
             {images.map((src, index) => (
-              <CarouselItem className="basis-1/4" key={index}>
-                <a
-                  href={src}
-                  // href={IMAGE_BOOK_URL + src}
-                  className="glightbox"
-                  data-gallery="gallery"
-                >
-                  <Image
-                    width={100}
-                    height={100}
-                    className="w-full aspect-[1/1] hover:scale-95 transition-transform duration-500 ease-in-out object-contain p-0.5 rounded-md cursor-pointer"
-                    // src={IMAGE_BOOK_URL + src}
-                    src={src}
-                    alt={`Thumbnail ${index + 1}`}
-                    loading="lazy" // Lazy load thumbnails
-                  />
-                </a>
+              <CarouselItem
+                className="basis-1/4 glightbox"
+                key={index}
+                href={src}
+                data-gallery="gallery"
+              >
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-full aspect-[1/1] hover:scale-95 transition-transform duration-500 ease-in-out object-contain p-0.5 rounded-md cursor-pointer"
+                  // src={IMAGE_BOOK_URL + src}
+                  src={src}
+                  alt={`Thumbnail ${index + 1}`}
+                  loading="lazy" // Lazy load thumbnails
+                />
               </CarouselItem>
             ))}
           </CarouselContent>

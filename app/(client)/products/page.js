8fecadc7d66421f40298b0299ac2b8b3dfc6b433
kimkim168 <KimKim168>
2024-12-10 17:13:13 +0700

    update

--- app/(client)/products/page.js
+++ app/(client)/products/page.js
@@ -1,19 +1,19 @@
 "use client";
-import React from "react";
+// import React from "react";
 // import GLightbox from "glightbox";
-import "glightbox/dist/css/glightbox.css";
+// import "glightbox/dist/css/glightbox.css";
 
-import Link from "next/link";
-import Image from "next/image";
-import { Heart } from "lucide-react";
-import MyGallery from "@/components/MyGallery";
-import { Button } from "@/components/ui/button";
-import MyVideoCard from "@/components/MyVideoCard";
-import MyNewsProduct from "@/components/MyNewsProduct";
-import MyBreadCrumb from "@/components/my-bread-crumb";
-import DescriptionTab from "@/components/products/description-tab";
+// import Link from "next/link";
+// import Image from "next/image";
+// import { Heart } from "lucide-react";
+// import MyGallery from "@/components/MyGallery";
+// import { Button } from "@/components/ui/button";
+// import MyVideoCard from "@/components/MyVideoCard";
+// import MyNewsProduct from "@/components/MyNewsProduct";
+// import MyBreadCrumb from "@/components/my-bread-crumb";
+// import DescriptionTab from "@/components/products/description-tab";
 
-export default function MyProduct() {
+export default function Page() {
   return (
     <>
       <p>Hello World</p>

75d04a597f1bc9c0e84020e5a621f714b66f4fc1
kimkim168 <KimKim168>
2024-12-11 00:28:43 +0700

    MyGallery-error

--- app/(client)/products/page.js
+++ app/(client)/products/page.js
@@ -15,8 +15,8 @@ export default function MyProduct() {
         <div className="mt-3">
           <div className="flex flex-col sm:grid grid-cols-12 gap-12">
             <div className="sm:col-span-12 md:col-span-5">
-              <MyGallery />
-              For Image
+              {/* <MyGallery /> */}
+              Error 1 
             </div>
 
             <div className=" sm:col-span-12  md:col-span-7 text-blue xl:ml-5">

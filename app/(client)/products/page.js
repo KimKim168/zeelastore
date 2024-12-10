f1ffb5efa7550457b7d48994431ca62cb1f90dda
kimkim168 <KimKim168>
2024-12-10 23:45:22 +0700

    up-MyGallery

--- app/(client)/products/page.js
+++ app/(client)/products/page.js
@@ -1,4 +1,5 @@
 import MyBreadCrumb from "@/components/my-bread-crumb";
+import MyGallery from "@/components/MyGallery";
 import React from "react";
 
 export default function MyProduct() {
@@ -6,6 +7,11 @@ export default function MyProduct() {
     <>
       <section className="max-w-screen-2xl mb-10 mx-auto px-2 xl:px-20 mt-5 ">
         <MyBreadCrumb />
+        <div className="flex flex-col sm:grid grid-cols-12 gap-12">
+          <div className="sm:col-span-12 md:col-span-5">
+            <MyGallery />
+          </div>
+        </div>
       </section>
     </>
   );

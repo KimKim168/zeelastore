667ab9bb21aa677074f3ee0830cf3be4006c72f6
kimkim168 <KimKim168>
2024-12-11 09:46:53 +0700

    up

--- app/(client)/products/page.js
+++ app/(client)/products/page.js
@@ -17,7 +17,8 @@ export default function MyProduct() {
         <div className="mt-3">
           <div className="flex flex-col sm:grid grid-cols-12 gap-12">
             <div className="sm:col-span-12 md:col-span-5">
-              <MyGallery />
+              {/* <MyGallery /> */}
+              Error View images
             </div>
 
             <div className=" sm:col-span-12  md:col-span-7 text-blue xl:ml-5">

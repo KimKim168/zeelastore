f3a16903c7f4cace91949bd0de0f92ac8de5df03
kimkim168 <KimKim168>
2024-12-11 10:49:49 +0700

    up

--- components/MyGallery.js
+++ components/MyGallery.js
@@ -1,16 +1,23 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import GLightbox from "glightbox";
-import "glightbox/dist/css/glightbox.min.css";
+import dynamic from "next/dynamic";
 import Image from "next/image";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
-// import { IMAGE_BOOK_URL } from "@/config/env";
-// import MyCarouselButton from "./ui/my-carousel-button";
+
+// Dynamically import GLightbox and Carousel components
+const GLightbox = dynamic(() => import("glightbox"), { ssr: false });
+const Carousel = dynamic(
+  () => import("@/components/ui/carousel").then((mod) => mod.Carousel),
+  { ssr: false }
+);
+const CarouselContent = dynamic(
+  () => import("@/components/ui/carousel").then((mod) => mod.CarouselContent),
+  { ssr: false }
+);
+const CarouselItem = dynamic(
+  () => import("@/components/ui/carousel").then((mod) => mod.CarouselItem),
+  { ssr: false }
+);
 
 const MyGallery = ({
   image = "/assets/images/product5.png",
@@ -25,20 +32,19 @@ const MyGallery = ({
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    // Set client-only rendering after the initial render
     setIsClient(true);
   }, []);
 
   useEffect(() => {
-    if (isClient) {
-      const lightbox = GLightbox({
-        touchNavigation: true,
-        loop: false,
-      });
-      return () => {
-        lightbox.destroy();
-      };
-    }
+    if (!isClient) return;
+    const lightbox = GLightbox({
+      touchNavigation: true,
+      loop: false,
+    });
+
+    return () => {
+      lightbox.destroy();
+    };
   }, [isClient]);
 
   if (!isClient) return null;
@@ -46,7 +52,6 @@ const MyGallery = ({
   return (
     <div className="max-w-[500px] mx-auto flex flex-col gap-4">
       <a
-        // href={IMAGE_BOOK_URL + image}
         href={image}
         className="glightbox"
         data-gallery="gallery"
@@ -56,7 +61,6 @@ const MyGallery = ({
           width={600}
           height={600}
           className="w-full transition-transform duration-500 rounded-md cursor-pointer hover:scale-105"
-          // src={IMAGE_BOOK_URL + image}
           src={image}
           alt="Book Cover"
         />
@@ -68,7 +72,6 @@ const MyGallery = ({
               <CarouselItem className="basis-1/4" key={index}>
                 <a
                   href={src}
-                  // href={IMAGE_BOOK_URL + src}
                   className="glightbox"
                   data-gallery="gallery"
                   aria-label={`View thumbnail ${index + 1}`}
@@ -77,16 +80,14 @@ const MyGallery = ({
                     width={100}
                     height={100}
                     className="w-full aspect-[1/1] hover:scale-95 transition-transform duration-500 ease-in-out object-contain p-0.5 rounded-md cursor-pointer"
-                    // src={IMAGE_BOOK_URL + src}
                     src={src}
                     alt={`Thumbnail ${index + 1}`}
-                    loading="lazy" // Lazy load thumbnails
+                    loading="lazy"
                   />
                 </a>
               </CarouselItem>
             ))}
           </CarouselContent>
-          {/* <MyCarouselButton /> */}
         </Carousel>
       )}
     </div>

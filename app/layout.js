19befa6ab3d4b7bf81081a06d49a0e8aad304b77
Long-Soeng167 <generalpurpose167@gmail.com>
2025-06-06 14:20:07 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ import MyLoadingAnimation from "@/components/my-loading-animation";
 import HomeHeader from "./components/home-header";
 import MyFloatTelegram from "@/components/my-float-telegram";
 import { BottomMobileNav } from "@/components/bottom-mobile-nav";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,9 +29,34 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager Script */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T77BPR3R');
+        `}
+        </Script>
+
+        {/* Your main component */}
+      </head>
       <body
         className={` ${roboto.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager NoScript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T77BPR3R"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        
         <MyFloatTelegram />
         <HomeHeader />
 
